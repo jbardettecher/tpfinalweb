2cddced9ac44ac168920c50ed6b7ce2641981c3e
Jordan BARDET--TECHER <jordan.bardettecher@etu.unilim.fr>
2025-12-10 10:14:16 +0100

    index fonctionnelle

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@
         <link rel="stylesheet" type="text/css" href="/public/css/home.css">
     </head>
     <na>
-        <a href="/posts/list">Voir les Posts</a> |
-        <a href="/user/login">Connexion</a>
-        <a href="/user/register">Inscription</a>
+        <a href="../Views/posts/list">Voir les Posts</a> |
+        <a href="../Views/user/login">Connexion</a>
+        <a href="../Views/user/register">Inscription</a>
     </nav>
     </nav>
     <body class="body-home">
