f497f6825a22ff7bf5a148460a767bba5e9e26cb
Jordan BARDET--TECHER <jordan.bardettecher@etu.unilim.fr>
2025-12-10 10:41:53 +0100

    correction des chemin de fichier sur la page principale

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@
         <link rel="stylesheet" type="text/css" href="/public/css/home.css">
     </head>
     <na>
-        <a href="../Views/posts/list.php">Voir les Posts</a> |
-        <a href="../Views/user/login.php">Connexion</a>
-        <a href="../Views/user/register.php">Inscription</a>
+        <a href="../src/Views/posts/list.php">Voir les Posts</a> |
+        <a href="../src/Views/user/login.php">Connexion</a>
+        <a href="../src/Views/user/register.php">Inscription</a>
     </nav>
     </nav>
     <body class="body-home">
